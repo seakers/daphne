de56f97d922136bb9d8880a8ee2d057055e89418
gapaza <gapaza@tamu.edu>
2020-02-15 14:21:51 -0600

    build_vassar.sh now build systemarchitecture problems and seakers/orekit

--- build/scripts/vassar/build_vassar.sh
+++ build/scripts/vassar/build_vassar.sh
@@ -3,19 +3,39 @@
 # Stop VASSAR if running on supervisorctl
 supervisorctl stop vassar
 
-# Clear gradle build caches
+# Clear gradle caches
 rm -rf $HOME/.gradle/caches/
 
-# Remove VASSAR_lib jars
-rm -rf $HOME/.m2/repository/seakers/vassar
 
-# Rebuild and publish VASSAR_lib
-cd /app/daphne/VASSAR_lib || exit
+
+# BUILD: seakers/system-architecture-problems
+echo "-----> Building seakers/system-architecture-problems"
+rm -rf $HOME/.m2/repository/seakers/system-architecture-problems
+cd /app/daphne/SystemArchitectureProblems
+mvn install
+
+
+# BUILD: seakers/orekit
+echo "-----> Building seakers/orekit"
+rm -rf $HOME/.m2/repository/seakers/orekit
+cd /app/daphne/orekit/orekit
+mvn install
+
+
+# BUILD: seakers/vassar
+echo "-----> Building seakers/vassar"
+rm -rf $HOME/.m2/repository/seakers/vassar
+cd /app/daphne/VASSAR_lib
 gradle publishToMavenLocal
 
-# Rebuild VASSAR_server
+
+# BUILD: VASSAR_server
+echo "-----> Building VASSAR_server"
 cd /app/daphne/VASSAR_server || exit
 gradle build
+
+
+
 
 # Start VASSAR with supervisorctl
 supervisorctl start vassar
