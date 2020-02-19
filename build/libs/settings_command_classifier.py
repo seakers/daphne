61af0f2194562247d2fafa5b8c4ce7054c1e5175
gapaza <gapaza@tamu.edu>
2020-02-19 11:52:58 -0600

    updated libs settings file for command classifier to work with environment variables

--- build/libs/settings_command_classifier.py
+++ build/libs/settings_command_classifier.py
@@ -4,8 +4,8 @@ import os
 
 DATABASE = {
     'drivername': 'postgres',
-    'host': 'postgres',
-    'port': '5432',
+    'host': os.environ['POSTGRES_HOST'],
+    'port': os.environ['POSTGRES_PORT'],
     'username': os.environ['USER'],
     'password': os.environ['PASSWORD'],
     'database': 'daphne'
