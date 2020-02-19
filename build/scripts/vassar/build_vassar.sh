3df00b0aeaf6bbfca41798cf638c6e64df70c518
gapaza <gapaza@tamu.edu>
2020-02-19 12:23:10 -0600

    can add -c flag to: daphne rebuild vassar... to clear the VASSAR_resources/orekit/CoverageDatabase

--- build/scripts/vassar/build_vassar.sh
+++ build/scripts/vassar/build_vassar.sh
@@ -6,25 +6,28 @@ supervisorctl stop vassar
 # Clear gradle caches
 rm -rf $HOME/.gradle/caches/
 
+# Clear maven local jars
+rm -rf $HOME/.m2/repository/seakers/system-architecture-problems
+rm -rf $HOME/.m2/repository/seakers/orekit
+rm -rf $HOME/.m2/repository/seakers/vassar
 
 
 # BUILD: seakers/system-architecture-problems
 echo "-----> Building seakers/system-architecture-problems"
-rm -rf $HOME/.m2/repository/seakers/system-architecture-problems
 cd /app/daphne/SystemArchitectureProblems
+mvn dependency:purge-local-repository
 mvn install
 
 
 # BUILD: seakers/orekit
 echo "-----> Building seakers/orekit"
-rm -rf $HOME/.m2/repository/seakers/orekit
 cd /app/daphne/orekit/orekit
+mvn dependency:purge-local-repository
 mvn install
 
 
 # BUILD: seakers/vassar
 echo "-----> Building seakers/vassar"
-rm -rf $HOME/.m2/repository/seakers/vassar
 cd /app/daphne/VASSAR_lib
 gradle publishToMavenLocal
 
