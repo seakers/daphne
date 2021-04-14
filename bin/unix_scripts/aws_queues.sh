a7c687149c8389538eb70db526220c539d278dc8
Antoni Viros <ani300@gmail.com>
2021-04-14 18:06:12 -0500

    Updated repos, new queues

--- bin/unix_scripts/aws_queues.sh
+++ bin/unix_scripts/aws_queues.sh
@@ -1,2 +1,3 @@
-docker run --rm -it amazon/aws-cli --endpoint-url=http://localhost:4576 sqs create-queue --queue-name vassar_queue
+docker run --rm -it amazon/aws-cli --endpoint-url=http://localhost:4576 sqs create-queue --queue-name vassar_request
+docker run --rm -it amazon/aws-cli --endpoint-url=http://localhost:4576 sqs create-queue --queue-name vassar_response
 docker run --rm -it amazon/aws-cli --endpoint-url=http://localhost:4576 sqs create-queue --tags problem_id=5 --queue-name algorithm_queue
