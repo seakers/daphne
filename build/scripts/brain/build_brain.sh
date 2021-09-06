4e27a57c7e6046b8916a080590126c243e95282d
Antoni Viros <ani300@gmail.com>
2021-09-06 17:27:33 -0500

    Updated scripts for correct build

--- build/scripts/brain/build_brain.sh
+++ build/scripts/brain/build_brain.sh
@@ -34,23 +34,24 @@ fi
 
 supervisorctl start vassar
 
-# Index historian database + train models
+# Index historian database
 if [ "${1}" == "--all" ]; then
   # Index historical database
   chmod +x /app/scripts/brain/index_historian_database.sh
   bash /app/scripts/brain/index_historian_database.sh
-
-  # Train models
-  chmod +x /app/scripts/brain/update_command_models.sh
-  bash /app/scripts/brain/update_command_models.sh
 fi
 
 # Run migrations
+mkdir /app/daphne/daphne_brain/dialogue/models
 bash /app/scripts/brain/migrate_database.sh
 
 if [ "${1}" == "--all" ]; then
   # Index VASSAR database
   python /app/daphne/VASSAR_resources/db_utility/index.py
+
+  # Train models
+  chmod +x /app/scripts/brain/update_command_models.sh
+  bash /app/scripts/brain/update_command_models.sh
 fi
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "Please go to http://localhost:6002, put 'daphne' as a password if asked, then click on the DATA tab on top, and press 'track all' for all tables and foreign-key relationships. When done, press <ENTER> here."
